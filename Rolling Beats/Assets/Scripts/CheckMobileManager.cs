5cb349b5dde47be4d9416d590be8d4d4271342b8
DavidGanfornina <73431730+DavidGanfornina@users.noreply.github.com>
2021-10-28 12:37:22 +0200

    Revert "Controles Movil2"
    This reverts commit 9875a54d33ffbb38a2a4586fae72204c69dbb0e3.

--- Rolling Beats/Assets/Scripts/CheckMobileManager.cs
+++ Rolling Beats/Assets/Scripts/CheckMobileManager.cs
@@ -9,7 +9,6 @@ using UnityEngine;
 public class CheckMobileManager : MonoBehaviour
 {
     public static CheckMobileManager SharedInstance;
-    private bool isMobile;
 
     public bool IsMobileGet
     {
