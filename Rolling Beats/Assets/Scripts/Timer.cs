e9329e37d550f28209bc6927772015eeadcd3f85
juanjomalouu <abadjuanjo@hotmail.com>
2021-12-11 13:38:05 +0100

    Timer Bug Fixed :D
    Now the Timer stays filled when the song ends.

--- Rolling Beats/Assets/Scripts/Timer.cs
+++ Rolling Beats/Assets/Scripts/Timer.cs
@@ -8,9 +8,11 @@ public class Timer : MonoBehaviour
 {    
     private Image loadingBar;
     private float totalSongSeconds;
+    private bool clockTimes;
 
     private void Start()
     {
+        clockTimes = false;
         loadingBar = GetComponent<Image>();
         loadingBar.fillAmount = 0f; 
         totalSongSeconds = Conductor.instance.musicSource.clip.length;
@@ -18,11 +20,19 @@ public class Timer : MonoBehaviour
 
     private void Update()
     {
-        float passedTime = Conductor.instance.songPosition;
+        if (!clockTimes)
+        {
+            float passedTime = Conductor.instance.songPosition;
 
-        if(passedTime > 0.5)
-            loadingBar.fillAmount = passedTime/totalSongSeconds; 
+            if (passedTime > 0.5)
+            {
+                Debug.Log("Reloj: " + passedTime + "/" + totalSongSeconds + "=" + passedTime / totalSongSeconds);
+                loadingBar.fillAmount = passedTime / totalSongSeconds;
+                if (passedTime / totalSongSeconds >= 1)
+                {
+                    clockTimes = true;
+                }
+            }
+        }
     }
-    
-
 }
